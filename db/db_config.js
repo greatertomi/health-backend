c2b6715c11afc02e2dcae904d26cb6c52c832478
Oshalusi John <oshalusijohn@gmail.com>
2020-07-18 17:44:59 +0100

    Settting up for deployment on heroku

--- db/db_config.js
+++ db/db_config.js
@@ -2,5 +2,5 @@ module.exports = {
   host: 'us-cdbr-east-02.cleardb.com',
   user: 'bf10232790d9d4',
   password: '1cc15d4e',
-  database: 'heroku_45b4856ef8d7128?'
+  database: 'heroku_45b4856ef8d7128'
 };

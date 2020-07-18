9d115761e4da4efcb8ff20d7dc40093a1e7f3203
Oshalusi John <oshalusijohn@gmail.com>
2020-07-18 18:16:17 +0100

    Testing

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 
 const { host, user, password, database } = require('./db_config');
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host,
   user,
   password,

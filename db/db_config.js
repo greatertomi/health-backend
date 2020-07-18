f943e0b237ce66c047e84ffdf5c3464ae319a7fa
Oshalusi John <oshalusijohn@gmail.com>
2020-07-18 17:29:20 +0100

    Updated files for hosting on heroku

--- db/db_config.js
+++ db/db_config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'health'
+  host: 'us-cdbr-east-02.cleardb.com',
+  user: 'bf10232790d9d4',
+  password: '1cc15d4e',
+  database: 'heroku_45b4856ef8d7128?'
 };
